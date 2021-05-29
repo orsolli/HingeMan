20a338dd9b976ca9aa50bdc4738ff1795427c937
Ørjan Solli <orjansolli@outlook.com>
2021-05-29 18:34:37 +0200

    Start in zero-g

---
+++ Assets/Scripts/IncreaseGravity.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Unity.MLAgents;
+
+public class IncreaseGravity : MonoBehaviour
+{
+    public int timeToFullGravity = 1000000;
+    // Update is called once per frame
+    void Start()
+    {
+        Academy.Instance.AgentPreStep += UpdateGravity;
+    }
+    public void UpdateGravity(int stepCount)
+    {
+        Physics.gravity = new Vector3(0, -9.81f, 0) * Mathf.Clamp01((float)stepCount / timeToFullGravity);
+    }
+}
