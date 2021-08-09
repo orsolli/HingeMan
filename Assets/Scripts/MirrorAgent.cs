e6f7e6357ef9355e1551b545ee35400d04e7cc13
Ørjan Solli <orjansolli@outlook.com>
2021-08-09 08:12:58 +0200

    Fix mirrored feet

--- Assets/Scripts/MirrorAgent.cs
+++ Assets/Scripts/MirrorAgent.cs
@@ -91,12 +91,12 @@ public class MirrorAgent : Agent
         Quaternion lookAtLeft = Quaternion.LookRotation(focusPoint - absLeftEye, head.rotation * Vector3.up);
         sensor.AddObservation(lookAtLeft);
 
-        Transform rightFoot = body.transform.Find("RightFoot");
+        Transform rightFoot = body.transform.Find("LeftFoot");
         Quaternion rightFoot_rotation = Quaternion.Inverse(rightFoot.rotation).normalized;
         FootSensor rightFootSensor = rightFoot.GetComponent<FootSensor>();
         sensor.AddObservation(Vector3.ClampMagnitude(rightFoot_rotation * rightFootSensor.impulse / 20f, 1f));
 
-        Transform leftFoot = body.transform.Find("LeftFoot");
+        Transform leftFoot = body.transform.Find("RightFoot");
         Quaternion leftFoot_rotation = Quaternion.Inverse(leftFoot.rotation).normalized;
         FootSensor leftFootSensor = leftFoot.GetComponent<FootSensor>();
         sensor.AddObservation(Vector3.ClampMagnitude(leftFoot_rotation * leftFootSensor.impulse / 20f, 1f));
